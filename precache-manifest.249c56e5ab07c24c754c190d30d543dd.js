73834ead37ed4c5054e15ef68ff7d1789b57d90f
Nguyen Tan Nhut <tannhut2018@gmail.com>
2020-04-05 20:20:10 +0700

    Updates

---
+++ precache-manifest.249c56e5ab07c24c754c190d30d543dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb517eff078d258c49efef02b11e579",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "a0e7c58255c1a8e79218",
+    "url": "/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2d03f467392b8a374af",
+    "url": "/deploy-react-app/static/js/2.9f0cf563.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.9f0cf563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e7c58255c1a8e79218",
+    "url": "/deploy-react-app/static/js/main.303e545f.chunk.js"
+  },
+  {
+    "revision": "ebb32217c4ce3ef87f74",
+    "url": "/deploy-react-app/static/js/runtime-main.e0b1ef7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
